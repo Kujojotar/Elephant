eb39ccbc4786c21e692510cad127d71200f9b35f
niejian <niejian@localhost.localdomain>
2021-12-02 15:29:45 +0800

    c7 fi

--- main.c
+++ main.c
@@ -1,14 +1,8 @@
 #include "print.h"
 int main(void){
     put_str("I am kernel");
-    put_char('\n');
-    put_int(0);
-    put_char('\n');
-    put_int(9);
-    put_char('\n');
-    put_int(0x00021a3f);
-    put_char('\n');
-    put_int(0xffee3219);
+    init_all();
+    asm volatile("sti");
     while(1);
     return 0;
 }
